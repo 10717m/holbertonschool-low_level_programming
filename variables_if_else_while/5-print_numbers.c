aef328882915058f14c0145946784741c2884f47
mohammed <mohammedbz670@gmail.com>
2025-02-17 21:10:40 +0000

    C new

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all single-digit numbers of base 10
+ * starting from 0, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number;
+
+/* Loop through numbers 0 to 9 */
+for (number = 0; number < 10; number++)
+{
+/* Convert the number to its ASCII representation and print it */
+putchar(number + '0');
+}
+
+/* Print a new line */
+putchar('\n');
+
+return (0);
+}

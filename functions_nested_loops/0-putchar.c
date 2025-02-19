8490e805050ac24a7bf7b86288c11de89778be4f
mohammed <mohammedbz670@gmail.com>
2025-02-19 11:42:21 +0000

    c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints "_putchar", followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* Print '_putchar' character by character */
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+
+/* Print a new line */
+_putchar('\n');
+
+return (0);
+}

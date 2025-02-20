e808fdb0333df3399fb4a006d89ba72d1a023410
mohammed <mohammedbz670@gmail.com>
2025-02-20 21:09:14 +0000

    C

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -3,32 +3,31 @@
 /**
  * jack_bauer - Prints every minute of the day of Jack Bauer.
  *
- * Description: This function prints every minute of the day in the format HH:MM,
  * starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
-    int hour, minute;
+int hour, minute;
 
-    /* Loop through hours (0 to 23) */
-    for (hour = 0; hour < 24; hour++)
-    {
-        /* Loop through minutes (0 to 59) */
-        for (minute = 0; minute < 60; minute++)
-        {
-            /* Print the hour (two digits) */
-            _putchar((hour / 10) + '0'); /* First digit of hour */
-            _putchar((hour % 10) + '0'); /* Second digit of hour */
+/* Loop through hours (0 to 23) */
+for (hour = 0; hour < 24; hour++)
+{
+/* Loop through minutes (0 to 59) */
+for (minute = 0; minute < 60; minute++)
+{
+/* Print the hour (two digits) */
+_putchar((hour / 10) + '0'); /* First digit of hour */
+_putchar((hour % 10) + '0'); /* Second digit of hour */
 
-            /* Print the colon */
-            _putchar(':');
+/* Print the colon */
+_putchar(':');
 
-            /* Print the minute (two digits) */
-            _putchar((minute / 10) + '0'); /* First digit of minute */
-            _putchar((minute % 10) + '0'); /* Second digit of minute */
+/* Print the minute (two digits) */
+_putchar((minute / 10) + '0'); /* First digit of minute */
+_putchar((minute % 10) + '0'); /* Second digit of minute */
 
-            /* Print a new line */
-            _putchar('\n');
-        }
-    }
-} 
+/* Print a new line */
+_putchar('\n');
+}
+}
+}

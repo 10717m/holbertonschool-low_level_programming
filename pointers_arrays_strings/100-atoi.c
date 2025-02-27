203fa25be8cdb481b40673bf6263699fbe07f5d2
mohammed <mohammedbz670@gmail.com>
2025-02-27 23:48:00 +0000

    C

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: A pointer to the string.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+int sign = 1; /* Initialize sign as positive */
+int result = 0; /* Initialize result */
+int i = 0;
+
+/* Skip leading spaces */
+while (s[i] == ' ')
+{
+i++;
+}
+
+/* Handle signs */
+if (s[i] == '-' || s[i] == '+')
+{
+if (s[i] == '-')
+{
+sign = -1; /* Negative sign */
+}
+i++;
+}
+
+/* Convert digits to integer */
+while (s[i] >= '0' && s[i] <= '9')
+{
+/* Check for overflow */
+if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
+{
+return (sign == 1 ? INT_MAX : INT_MIN);
+}
+
+result = result * 10 + (s[i] - '0');
+i++;
+}
+
+return (result * sign); /* Apply the sign */
+}   
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 /* Function prototypes */
-char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /* MAIN_H */

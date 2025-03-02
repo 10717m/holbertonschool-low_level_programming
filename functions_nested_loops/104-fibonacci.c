0ade99fecc2ed66cfc201447681d9f9a0cbe4c42
Your Name <you@example.com>
2025-03-02 10:22:13 +0000

    Fix: Correctly handle large Fibonacci numbers in 104-fibonacci.c

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -8,18 +8,30 @@
 int main(void)
 {
 int i;
-unsigned long int a = 1, b = 2, next;
+unsigned long int a1 = 0, a2 = 1, b1 = 0, b2 = 2;
+unsigned long int next1, next2;
 
-printf("%lu, %lu", a, b);
+printf("%lu, %lu", b2, a2 + b2);
 
 for (i = 2; i < 98; i++)
 {
-next = a + b;
-printf(", %lu", next);
-a = b;
-b = next;
+next1 = a1 + b1;
+next2 = a2 + b2;
+
+if (next2 < b2)
+{
+next1 += 1;
+}
+
+a1 = b1;
+a2 = b2;
+b1 = next1;
+b2 = next2;
+
+printf(", %lu%lu", b1, b2);
 }
 
 printf("\n");
+
 return (0);
 }

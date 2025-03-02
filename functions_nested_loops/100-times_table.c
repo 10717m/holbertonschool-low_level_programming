fbc8e9ff7e3d04ca008badda536492a7df3a7bf2
Your Name <you@example.com>
2025-03-02 09:57:12 +0000

    Add 100-times_table.c program

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -7,16 +7,13 @@
 void print_times_table(int n)
 {
 int i, j, result;
-
 if (n < 0 || n > 15)
 return;
-
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
 result = i * j;
-
 if (j == 0)
 {
 _putchar('0' + result);
@@ -25,7 +22,6 @@ else
 {
 _putchar(',');
 _putchar(' ');
-
 if (result < 10)
 {
 _putchar(' ');

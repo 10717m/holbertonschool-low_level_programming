bc9316b9c31bc406e9838911f65ca757b13cb8af
mohammed <mohammedbz670@gmail.com>
2025-03-03 19:38:49 +0000

    C

--- pointers_arrays_strings/main_0.c
+++ pointers_arrays_strings/main_0.c
@@ -1,29 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-int nb;
+char *s = "hello";
+char *f;
 
-nb = _atoi("98");
-printf("%d\n", nb);
-nb = _atoi("-402");
-printf("%d\n", nb);
-nb = _atoi("          ------++++++-----+++++--98");
-printf("%d\n", nb);
-nb = _atoi("214748364");
-printf("%d\n", nb);
-nb = _atoi("0");
-printf("%d\n", nb);
-nb = _atoi("Suite 402");
-printf("%d\n", nb);
-nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-printf("%d\n", nb);
-nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-printf("%d\n", nb);
+f = _strchr(s, 'l');
+
+if (f != NULL)
+{
+printf("%s\n", f);
+}
 return (0);
 }

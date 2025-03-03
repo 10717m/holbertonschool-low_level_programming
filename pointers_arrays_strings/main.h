4299b1b4caa01fe46b462b93ed3aec839a5a21e4
mohammed <mohammedbz670@gmail.com>
2025-03-03 19:27:43 +0000

    C

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototype for _memset */
 char *_memset(char *s, char b, unsigned int n);
 
-#endif
+/* Function prototype for _memcpy */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* Function prototype for _putchar (provided by the environment) */
+int _putchar(char c);
+
+#endif /* MAIN_H */

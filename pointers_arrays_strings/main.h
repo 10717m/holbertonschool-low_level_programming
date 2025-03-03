2afee8e3ecf79213e79047964588915f2ae678f8
mohammed <mohammedbz670@gmail.com>
2025-03-03 19:32:08 +0000

    C

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,6 +7,9 @@ char *_memset(char *s, char b, unsigned int n);
 /* Function prototype for _memcpy */
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+/* Function prototype for _strchr */
+char *_strchr(char *s, char c);
+
 /* Function prototype for _putchar (provided by the environment) */
 int _putchar(char c);
 

2a01d7b59614840da5b4eda712698a813851d78c
mohammed <mohammedbz670@gmail.com>
2025-03-03 20:02:40 +0000

    C

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -5,17 +5,18 @@
  * @s: Pointer to the string to search
  * @accept: Pointer to the string containing accepted characters
  *
- * Return: bytes in the initial segment of consisting only of bytes from accept
+ * Return: Number of bytes in the initial segment of s consisting only  accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int count = 0;
 int found;
+char *a; /* Declare the variable at the beginning of the block */
 
 while (*s)
 {
 found = 0;
-char *a = accept;
+a = accept; /* Assign the value here */
 
 while (*a)
 {

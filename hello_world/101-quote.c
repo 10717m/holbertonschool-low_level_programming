9d931c4fa1508102f9d670b96ac28a77b0bf235f
mohammed <mohammedbz670@gmail.com>
2025-03-04 20:40:52 +0000

    c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, 59); /* 59 is the length of the message including the newline */
+const char *message = "and that piece is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 59); /* 59 is the length  message including the newline */
 return (1);
 }

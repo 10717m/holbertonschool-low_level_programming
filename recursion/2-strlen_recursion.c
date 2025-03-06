190c5f03f2da49a0104f3080679a7ac2eb2a8356
mohammed <mohammedbz670@gmail.com>
2025-03-06 15:57:19 +0000

    c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -13,5 +13,5 @@ if (*s == '\0') /* Base case: end of string */
 return (0);
 }
 
-return (1 + _strlen_recursion(s + 1)); /* Recursively call with the next character */
+return (1 + _strlen_recursion(s + 1)); /* Recursively cal with the character */
 }

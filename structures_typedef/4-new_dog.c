44ce08295c436445633d8fa6ea3ff0af40b50ca3
mohammed <mohammedbz670@gmail.com>
2025-03-15 00:41:24 +0000

    c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,30 +15,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *new_dog;
 char *name_copy, *owner_copy;
+size_t name_len, owner_len;
 
 /* Allocate memory for the dog_t structure */
 new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
 
+/* Calculate lengths of name and owner */
+name_len = strlen(name);
+owner_len = strlen(owner);
+
 /* Allocate memory and copy the name */
-name_copy = malloc(strlen(name) + 1);
+name_copy = malloc(name_len + 1);
 if (name_copy == NULL)
 {
 free(new_dog);
 return (NULL);
 }
-strcpy(name_copy, name);
+for (size_t i = 0; i <= name_len; i++)
+name_copy[i] = name[i];
 
 /* Allocate memory and copy the owner */
-owner_copy = malloc(strlen(owner) + 1);
+owner_copy = malloc(owner_len + 1);
 if (owner_copy == NULL)
 {
 free(name_copy);
 free(new_dog);
 return (NULL);
 }
-strcpy(owner_copy, owner);
+for (size_t i = 0; i <= owner_len; i++)
+owner_copy[i] = owner[i];
 
 /* Assign values to the new dog */
 new_dog->name = name_copy;

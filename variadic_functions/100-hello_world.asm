cf4c6ecceb4938f3ac00bf05b6ed56559a182f53
mohammed <mohammedbz670@gmail.com>
2025-04-08 11:42:49 +0000

    c

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,19 +1,19 @@
 section .data
-msg db 'Hello, World', 10   ; 'Hello, World' followed by newline (ASCII 10)
-len equ $ - msg             ; Calculate the length of the message
+    msg db 'Hello, World', 10   ; Message + newline (ASCII 10)
+    len equ $ - msg             ; Length calculation
 
 section .text
-global _start
+    global _start
 
 _start:
-; Syscall: write(1, msg, len)
-mov rax, 1      ; syscall number for write (1)
-mov rdi, 1      ; file descriptor (1 = stdout)
-mov rsi, msg    ; pointer to message
-mov rdx, len    ; message length
-syscall         ; invoke syscall
+    ; write(1, msg, len)
+    mov rax, 1      ; syscall: write
+    mov rdi, 1      ; fd: stdout
+    mov rsi, msg    ; buffer
+    mov rdx, len    ; length
+    syscall
 
-; Syscall: exit(0)
-mov rax, 60     ; syscall number for exit (60)
-mov rdi, 0      ; exit status (0 = success)
-syscall         ; invoke syscall
+    ; exit(0)
+    mov rax, 60     ; syscall: exit
+    xor rdi, rdi    ; status: 0
+    syscall
